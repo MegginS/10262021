f2823f221cdb5fe9e2f6b5c78f5c81444f0b6fdb
Meggin <megginsimon@gmail.com>
2021-11-01 23:10:05 +0000

    updated

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,18 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_names, melon_seedlessness, melon_prices, melon_flesh_color, melon_rind_color, melon_average_weight
 
 
-def print_melon(name, seedless, price):
+def print_melon(name, seedless, price, flesh_color, rind_color, average_weight):
     """Print each melon with corresponding attribute information."""
 
     have_or_have_not = 'have'
     if seedless:
         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    print(f'{name}s are {flesh_color} on outside, {rind_color} inside, they {have_or_have_not} seeds, weigh {average_weight} and are ${price:.2f}')
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i], melon_flesh_color[i], melon_rind_color[i], melon_average_weight[i])
--- melons.py
+++ melons.py
@@ -21,3 +21,27 @@ melon_seedlessness = {
     4: False,
     5: False,
 }
+
+melon_flesh_color = {
+    1: "fcolor1",
+    2: "fcolor2",
+    3: "fcolor3",
+    4: "fcolor4",
+    5: "fcolor5",
+}
+
+melon_rind_color = {
+    1: "rcolor1",
+    2: "rcolor2",
+    3: "rcolor3",
+    4: "rcolor4",
+    5: "rcolor5",
+}
+
+melon_average_weight = {
+    1: "aw1",
+    2: "aw2",
+    3: "aw3",
+    4: "aw4",
+    5: "aw5",
+}
